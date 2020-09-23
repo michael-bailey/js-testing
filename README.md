84b33dea4075a9093235cba3c353631a14804cf1
Michael Bailey <michaelbailey1@fb.com>
2020-09-23 16:23:25 +0100

    added larger tree

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # js-testing
+
 ![Node.js CI](https://github.com/michael-bailey/js-testing/workflows/Node.js%20CI/badge.svg)
+
 a repo for trying jest unit testing
--- src/FamilyTree.test.js
+++ src/FamilyTree.test.js
@@ -1,4 +1,5 @@
 const {Person} = require('./FamilyTree')
+const util = require('util')
 
 describe('person tests', () => {
     test('create person', () => {
@@ -11,8 +12,8 @@ describe('person tests', () => {
     })
 
     test('add a parent to a child', () => {
-        let parent1 = new Person("michael", 19)
-        let child1 = new Person("alice", 45)
+        let parent1 = new Person("alice", 19)
+        let child1 = new Person("michael", 45)
 
         let result = child1.addParent(parent1)
         expect(result).toEqual(0)
@@ -20,14 +21,55 @@ describe('person tests', () => {
         expect(child1.parents[0]).toEqual(parent1)
     })
 
+    test('add too many parents to a child', () => {
+        let parent1 = new Person("alice", 19)
+        let child1 = new Person("michael", 45)
+
+        child1.addParent(parent1)
+        child1.addParent(parent1)
+        let result = child1.addParent(parent1)
+        expect(result).toEqual(1)
+    })
+
     test('add a parent to a child', () => {
-        let parent1 = new Person("michael", 19)
-        let child1 = new Person("alice", 45)
+        let parent1 = new Person("alice", 19)
+        let child1 = new Person("michael", 45)
 
         let result = parent1.addChild(child1)
         expect(result).toEqual(0)
 
         expect(child1.parents[0]).toEqual(parent1)
         expect(parent1.children[0]).toEqual(child1)
+
+        console.log(util.inspect(parent1))
+    })
+
+    test('large tree', () => {
+
+        // create many people
+        Aliahna = new Person("Aliahna", 2)
+        Carols = new Person("Carols", 3)
+
+        Trinise = new Person("Trinise", 30)
+        Shakur = new Person("Shakur", 33)
+
+        Mahlah = new Person("Mahlah", 67)
+        Dametris = new Person("Dametris", 64)
+        Shontrell = new Person("Shontrell", 66)
+        Naor = new Person("Naor", 65)
+
+        Mahlah.addChild(Trinise)
+        Dametris.addChild(Trinise)
+
+        Shontrell.addChild(Shakur)
+        Naor.addChild(Shakur)
+
+        Trinise.addChild(Aliahna)
+        Trinise.addChild(Carols)
+
+        Shakur.addChild(Aliahna)
+        Shakur.addChild(Carols)
+
+        console.log(util.inspect(Aliahna, {depth: 5}))
     })
 })
